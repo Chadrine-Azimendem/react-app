503b9fb6fc1295e8de1804a0c93507191d7d8d99
Chadrine-Azimendem <chadrine.azimendem@gmail.com>
2022-11-23 16:53:09 +0000

    add, subtrack buttons and states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import "./App.css";
-import React from "react";
 import fate from "./images/netflixFate.png";
 import gambit from "./images/netflixGambit.png";
 import outside from "./images/netflixOutside.png";
@@ -15,6 +15,28 @@ import bbcmorgan from "./images/bbcMorgan.png";
 import bbcunilever from "./images/bbcUnilever.png";
 
 const App = () => {
+  // const [toggle, settoggle] = useState(false);
+  let [number, setnumber] = useState(0);
+  const addNumber = () => {
+    setnumber((number += 1));
+  };
+  const subtractNumber = () => {
+    setnumber((number -= 1));
+  };
+
+  // const handleToggle = () => {
+  //   settoggle(!toggle);
+  // };
+  // // this is the least common way of conditionally rendering something
+  // if (toggle) {
+  //   return (
+  //     <div>
+  //       <h1>Good Bye!</h1>
+  //       <button onClick={handleToggle}>CLICK ME!</button>
+  //     </div>
+  //   );
+  // }
+
   return (
     <React.Fragment>
       <div className="container">
@@ -24,13 +46,13 @@ const App = () => {
         <Movies movieImg={gambit} />
         <Movies movieImg={witcher} />
       </div>
-      <div className="container">
+      <div className="container2">
         <Vacation type="Entire homes" locationImg={homes} />
         <Vacation type="Unique stays" locationImg={uniquestays} />
         <Vacation type="Cabins and cottages" locationImg={cabines} />
         <Vacation type="pets allowed" locationImg={petsallowed} />
       </div>
-      <div className="container">
+      <div className="container2">
         <News text="Unlocking to quickly..." newsImg={pandemic} />
         <News text="NHS england..." newsImg={bbcpay} />
         <News
@@ -41,6 +63,30 @@ const App = () => {
           text="The owner of Dove and Vaseline will ..."
           newsImg={bbcunilever}
         />
+      </div>
+      {/* <div>
+        {toggle ? <h1>Hello</h1> : <h1>Goodbye</h1>}
+        ternary operator is a better way of creating conditional rendering when
+        we have if or else
+        <button className={toggle ? "yellow" : "green"} onClick={handleToggle}>
+          CLICK ME!
+        </button>
+        {toggle && (
+          <div>
+            <h3>This is a pop-up</h3>
+            <p>some cool text</p>
+          </div>
+        )}
+      </div> */}
+
+      <div className="toggle-btn">
+        <button className="btn" onClick={subtractNumber}>
+          -
+        </button>
+        <h1>{number}</h1>
+        <button className="btn" onClick={addNumber}>
+          +
+        </button>
       </div>
     </React.Fragment>
   );
